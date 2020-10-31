49744353576ecc6d3b3a01789b1a918db6db7dfe
Pitunetix <griusech89@gmail.com>
2020-10-31 20:51:49 -0300

    Fomateo y quito los console.log que utilice

--- src/Components/TaskForm/TaskForm.js
+++ src/Components/TaskForm/TaskForm.js
@@ -1,71 +1,65 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { db } from "../../firebase";
 
 const TaskForm = ({ getTasks }) => {
 
-    // console.log(getTasks)
+  const [task, setTask] = useState({
+    status: 1,
+  });
 
-    const [task, setTask] = useState({
-        status: 1
+  const addTask = async (taksObject) => {
+    await db.collection("tasks").doc().set(taksObject);
+  };
+
+  const { title, desc } = task;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(task);
+    addTask(task);
+    getTasks();
+    setTask({
+      title: "",
+      desc: "",
+      status: 1,
     });
+  };
 
-    const addTask = async (taksObject) => {
-        // console.log(status)
-        await db.collection("tasks").doc().set(taksObject);
-        };
+  const handeInput = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-      const { title, desc } = task;
+  return (
+    <div className="col-md-5 m-auto">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="title"
+            placeholder="Titulo de la tarea"
+            className="form-control m-2"
+            onChange={handeInput}
+            value={title}
+          />
 
-      const handleSubmit = e => {
-          e.preventDefault();
-          console.log(task)
-          addTask(task)
-          getTasks()
-          setTask({
-              title: '',
-              desc: '',
-              status: 1
-          });
-      }
-  
-      const handeInput = e => {
-          setTask({
-              ...task,
-              [e.target.name] : e.target.value
-          })
-      }  
+          <input
+            type="text"
+            name="desc"
+            placeholder="Descripción"
+            className="form-control m-2"
+            onChange={handeInput}
+            value={desc}
+          />
+        </div>
+        <button type="submit" className="btn btn-secondary">
+          Agregar Tarea
+        </button>
+      </form>
+    </div>
+  );
+};
 
-    return (
-        
-            <div className="col-md-5 m-auto">
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <input 
-                    type="text"
-                    name="title"
-                    placeholder="Titulo de la tarea"
-                    className="form-control m-2"
-                    onChange={handeInput}
-                    value={title}
-                    />
-
-                    <input 
-                    type="text"
-                    name="desc"
-                    placeholder="Descripción"
-                    className="form-control m-2"
-                    onChange={handeInput}
-                    value={desc}
-                    />
-                </div>
-                <button 
-                type="submit"    
-                className="btn btn-secondary"
-                >Agregar Tarea</button>
-            </form>
-            </div>
-        
-    )
-}
-
-export default TaskForm
+export default TaskForm;
--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -1,55 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import TaskForm from '../TaskForm/TaskForm'
-import TaskStates from '../TaskStates/TaskStates'
+import React, { useState, useEffect } from "react";
+import TaskForm from "../TaskForm/TaskForm";
+import TaskStates from "../TaskStates/TaskStates";
 import { db } from "../../firebase";
 
-
 const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
 
-    const [tasks, setTasks] = useState([])
+  const status = [
+    { title: "ToDo", statusId: 1 },
+    { title: "In Progress", statusId: 2 },
+    { title: "Done", statusId: 3 },
+    { title: "Deleted", statusId: 4 },
+  ];
 
-    const status = [
-        {title: "ToDo", statusId: 1},
-        {title: "In Progress", statusId: 2},
-        {title: "Done", statusId: 3},
-        {title: "Deleted", statusId: 4},
-    ]
+  const getTasks = () => {
+    db.collection("tasks").onSnapshot((querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setTasks(docs);
+    });
+  };
 
-    const getTasks = () => {
-        db.collection("tasks").onSnapshot((querySnapshot) => {
-                const docs = [];
-                querySnapshot.forEach((doc) => {
-                docs.push({ ...doc.data(), id: doc.id });
-            });
-            setTasks(docs);
-        });
+  useEffect(() => {
+    getTasks();
+  }, []);
 
-    };
-    
-    // console.log(tasks)
-    useEffect(() => {
-        getTasks();
-    }, []);
+  return (
+    <div>
+      <TaskForm getTasks={getTasks} />
 
-    return (
-        <div>
-            <TaskForm getTasks={getTasks} />
+      <div className="row">
+        {status.map((state) => (
+          <TaskStates
+            tasks={tasks}
+            title={state.title}
+            getTasks={getTasks}
+            statusId={state.statusId}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            <div className="row">
-
-                {status.map(state =>
-                    <TaskStates 
-                        tasks={tasks} 
-                        title={state.title} 
-                        getTasks={getTasks}
-                        statusId={state.statusId}
-                    />
-                    )}
-            </div>
-
-
-        </div>
-    )
-}
-
-export default Tasks
+export default Tasks;
